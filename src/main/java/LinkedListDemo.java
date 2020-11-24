da75bb7d70e3763465aaf75b9c5a10b74b033d91
pravinnikure <pravinnikure300@gmail.com>
2020-11-24 13:54:56 +0530

    Worked with user defined class

--- src/main/java/LinkedListDemo.java
+++ src/main/java/LinkedListDemo.java
@@ -1,6 +1,8 @@
+import java.sql.SQLOutput;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LinkedListDemo
 {
@@ -21,10 +23,23 @@ public class LinkedListDemo
             System.out.println(itr.next());
         }
         list.add(1,"Z");
-        list.remove("Z")
+        list.remove("Z");
 
         System.out.println(list);
 
 
+        //User defined class example
+        List<Student> st =new LinkedList<Student>();
+
+        Student s1 = new Student(1,"Raj",22);
+        Student s3 = new Student(3,"Abhi",23);
+        Student s2 = new Student(2,"Ani",24);
+
+        st.add(s1);
+        st.add(s2);
+        st.add(s3);
+        for(Student s: st){
+            System.out.println(s.rollno+" "+s.name+" "+s.rollno);
+        }
     }
 }
