a607b5089223e0f1e4e9ac9a50ddde74297db31a
pravinnikure <pravinnikure300@gmail.com>
2020-11-24 12:10:55 +0530

    Simple demo operations

--- src/main/java/LinkedListDemo.java
+++ src/main/java/LinkedListDemo.java
@@ -1,2 +1,15 @@
-public class LinkedListDemo {
+import java.util.LinkedList;
+
+public class LinkedListDemo
+{
+    public static void main(String[] args)
+    {
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        System.out.println(list);
+    }
 }
