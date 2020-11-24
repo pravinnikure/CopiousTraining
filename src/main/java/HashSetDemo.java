1e4f8aa727b3a4233b6652693c0889b05ce7ab03
pravinnikure <pravinnikure300@gmail.com>
2020-11-24 15:29:17 +0530

    Hashset from another collections

--- src/main/java/HashSetDemo.java
+++ src/main/java/HashSetDemo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -9,10 +10,30 @@ public class HashSetDemo
         set.add("Mango");
         set.add("Banana");
         set.add("Papaya");
+        set.add("Grapes");
+
+        set.remove("Mango");
         Iterator<String> itr =set.iterator();
         while (itr.hasNext())
         {
             System.out.println(itr.next());
         }
+
+        //Hashset from another collections
+        ArrayList<Integer> list =new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.add(2);
+        list.add(3);
+        HashSet<Integer> has = new HashSet(list);
+        has.add(12);
+        Iterator<Integer> itre = has.iterator();
+        while (itre.hasNext()) {
+            System.out.println(itre.next());
+        }
+
+
     }
 }
