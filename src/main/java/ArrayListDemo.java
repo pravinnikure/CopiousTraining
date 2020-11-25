aff4a656a4c864a0a97dcf1992256f715706b260
pravinnikure <pravinnikure300@gmail.com>
2020-11-25 14:40:47 +0530

    deffrent way to iterate arraylist

--- src/main/java/ArrayListDemo.java
+++ src/main/java/ArrayListDemo.java
@@ -5,69 +5,87 @@ import java.util.*;
 
 public class ArrayListDemo
 {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-
-        list.remove(3);
-
-        System.out.println(list.contains(3));
-
-        System.out.println(list.size());
-
-        list.set(2,10);
-
-        for(int i:list)
+    public static void main(String[] args)
+    {
+        List<Integer> values = Arrays.asList(1,3,2,4,5,6);
+        for(int i=0;i<values.size();i++)
         {
-            System.out.println(" "+i);
+            System.out.println(i);
         }
-
-        Iterator iterator = list.iterator();
-        while (iterator.hasNext())
+        Iterator<Integer> itr = values.iterator();
+        while(itr.hasNext())
         {
-            System.out.println(iterator.next());
+            System.out.println(itr.next());
         }
-        System.out.println(list.clone());
-
-        //Sorting array
-
-        List<String> list1 = new ArrayList<String>();
-        list1.add("Mango");
-        list1.add("Apple");
-        list1.add("Banana");
-        list1.add("Grapes");
-
-        Collections.sort(list1);
-
-        for(String i : list1)
+        for(int i:values)
         {
             System.out.println(i);
         }
 
-        System.out.println("User defined class objects");
-
-        Student s1 = new Student(1,"Sanju",22);
-        Student s2 = new Student(2,"Anju",23);
-        Student s3 = new Student(4,"Manju",25);
-
-        ArrayList<Student> student  = new ArrayList<Student>();
-        student.add(s1);
-        student.add(s2);
-        student.add(s3);
-
-        Iterator iter = student.iterator();
-
-        while (iter.hasNext())
-        {
-            Student st = (Student)iter.next();
-            System.out.println(st.rollno+" "+st.name+" "+st.age);
-        }
 
 
+        //        ArrayList<Integer> list = new ArrayList();
+//
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//
+//        list.remove(3);
+//
+//        System.out.println(list.contains(3));
+//
+//        System.out.println(list.size());
+//
+//        list.set(2,10);
+//
+//        for(int i:list)
+//        {
+//            System.out.println(" "+i);
+//        }
+//
+//        Iterator iterator = list.iterator();
+//        while (iterator.hasNext())
+//        {
+//            System.out.println(iterator.next());
+//        }
+//        System.out.println(list.clone());
+//
+//        //Sorting array
+//
+//        List<String> list1 = new ArrayList<String>();
+//        list1.add("Mango");
+//        list1.add("Apple");
+//        list1.add("Banana");
+//        list1.add("Grapes");
+//
+//        Collections.sort(list1);
+//
+//        for(String i : list1)
+//        {
+//            System.out.println(i);
+//        }
+//
+//        System.out.println("User defined class objects");
+//
+//        Student s1 = new Student(1,"Sanju",22);
+//        Student s2 = new Student(2,"Anju",23);
+//        Student s3 = new Student(4,"Manju",25);
+//
+//        ArrayList<Student> student  = new ArrayList<Student>();
+//        student.add(s1);
+//        student.add(s2);
+//        student.add(s3);
+//
+//        Iterator iter = student.iterator();
+//
+//        while (iter.hasNext())
+//        {
+//            Student st = (Student)iter.next();
+//            System.out.println(st.rollno+" "+st.name+" "+st.age);
+//        }
+//
+//
     }
 
 }
