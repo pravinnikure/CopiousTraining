de530d8f03713fc53778f0043a24c25fd3d890dc
pravinnikure <pravinnikure300@gmail.com>
2020-11-26 19:43:06 +0530

    Map iteration using streams

--- src/main/java/StreamsDemo/MapStream.java
+++ src/main/java/StreamsDemo/MapStream.java
@@ -1,6 +1,7 @@
 package StreamsDemo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collector;
@@ -23,6 +24,13 @@ public class MapStream
         Map<Integer,String> map = list1.stream().filter(p->p.name.length()>4)
                 .collect(Collectors.toMap(p->p.empId,p->p.name));
         System.out.println(map);
+
+        Map<String,Integer> map1 = new HashMap<>();
+        map1.put("A",11);
+        map1.put("B",13);
+        map1.put("C",21);
+
+        map1.entrySet().stream().forEach(e->System.out.println(e.getKey()+" "+e.getValue()));
 
     }
 }
