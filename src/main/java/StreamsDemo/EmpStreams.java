10ae52dfb84dca774e6912da9ef667adce09af4b
pravinnikure <pravinnikure300@gmail.com>
2020-11-26 17:34:28 +0530

    Performed streams operation

--- src/main/java/StreamsDemo/EmpStreams.java
+++ src/main/java/StreamsDemo/EmpStreams.java
@@ -1,2 +1,43 @@
-package StreamsDemo;public class EmpStreams {
+package StreamsDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class EmpStreams
+{
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<Employee>();
+        //4 Employee created
+        Employee e1 = new Employee(01,"Robert",5000.0);
+        Employee e2 = new Employee(02,"Alen",7000.0);
+        Employee e3 = new Employee(03,"Deepak",15000.0);
+        Employee e4 = new Employee(04,"Vinod",3000.0);
+        //Employee added
+        list.add(e1);
+        list.add(e2);
+        list.add(e3);
+        list.add(e4);
+
+        //Iteration using for each loop
+        for(Employee i :list)
+        {
+            System.out.println(i.empId+" "+i.name+" "+i.salary);
+        }
+        //Iterating using streams
+        //Printing employee whose salary is greater than 50000
+        System.out.println("Id"+" "+"Name");
+        list.stream()
+                .filter(employe -> employe.salary>5000)
+                .forEach(employee->System.out.println(employee.empId+" "+employee.name+" "));
+
+//        List<Double> employees = list.stream()
+//                                   .filter(i ->i.salary>5000)
+//                                    .map(p->p.salary).collect(Collectors.toList());
+//        System.out.println(employees);
+
+
+
+    }
 }
